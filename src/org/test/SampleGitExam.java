8b8147cd45a7ec36d13f3456dd5a91a703fed13d
manikandanarun1907 <manikandanarun1907@gmail.com>
2026-03-17 11:04:38 +0530

    siva added few code

--- src/org/test/SampleGitExam.java
+++ src/org/test/SampleGitExam.java
@@ -1,8 +1,12 @@
 package org.test;
 
 public class SampleGitExam {
+	
+	private void m2() {
+		System.out.println("siva.....");
+	}
 	public static void main(String[] args) {
-		System.out.println("hello mani");
+		System.out.println("hello siva...");
 	}
 
 }
